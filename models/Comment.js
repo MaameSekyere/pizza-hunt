bd14cac5df16c9fd03185b6add6d036b886ade74
MaameSekyere <maameant@gmail.com>
2022-07-08 13:27:20 -0400

    add replies

--- models/Comment.js
+++ models/Comment.js
@@ -1,4 +1,28 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const ReplySchema = new Schema({
+  // set custom id to avoid confusion with parent comment _id
+  replyId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+
+  replyBody: {
+    type: String,
+  },
+  writtenBy: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal),
+  },
+  toJSON: {
+    getters: true,
+  },
+});
 
 const CommentSchema = new Schema({
   writtenBy: {
@@ -7,10 +31,25 @@ const CommentSchema = new Schema({
   commentBody: {
     type: String,
   },
+
+  // use ReplySchema to validate data for a reply
+
+  replies: [ReplySchema],
+
   createdAt: {
     type: Date,
     default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal),
   },
+  toJSON: {
+    getters: true,
+    virtuals: true,
+  },
+  id: false,
+});
+
+CommentSchema.virtual("replyCount").get(function () {
+  return this.replies.length;
 });
 
 const Comment = model("Comment", CommentSchema);
